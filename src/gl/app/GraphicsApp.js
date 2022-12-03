8429279579895a7951d95aaedd1b68584e5ae7f4
Lok Lok <yellowhappy831@gmail.com>
2022-12-04 07:31:26 +0800

    graphics

--- src/gl/app/GraphicsApp.js
+++ src/gl/app/GraphicsApp.js
@@ -5,13 +5,15 @@ import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
 export class GraphicsApp {
     constructor () {
         this.gl = new WebGLRenderer({alpha: true,})
+        this.scene = new Scene()
+        this.camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 500)
+       
         window.addEventListener('resize', () => {
             this.gl.setSize(window.innerWidth, window.innerHeight, true)
         })
         window.dispatchEvent(new CustomEvent('resize'))
         //
-        this.scene = new Scene()
-
+       
         new TextureLoader().load(`./hdri/brown_photostudio_05_1k.hdr`, (t) => {
             t.encoding = sRGBEncoding
             t.mapping = EquirectangularReflectionMapping
@@ -19,7 +21,6 @@ export class GraphicsApp {
             this.scene.environment = t
         })
 
-        this.camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 500)
         this.tm = taskManager
         this.tm.state = this
         document.querySelector('#root').appendChild(this.gl.domElement)
